100261d98bd65805d52c013b00c157928f172811
America Calleros <america.quintero@gmail.com>
2016-06-29 10:02:09 -0500

    Test full_name method

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,8 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      new_name = Person.new(first_name: 'America',last_name: 'Calleros')
+      expect(new_name.full_name).to eq('America Calleros')
     end
   end
 end
