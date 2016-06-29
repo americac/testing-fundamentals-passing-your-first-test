882e592998ebe7fd25d98a708bf1b5676345bcf9
America Calleros <america.quintero@gmail.com>
2016-06-29 10:03:58 -0500

    Change test name to Annabeth Chase

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,8 +3,8 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      new_name = Person.new(first_name: 'America',last_name: 'Calleros')
-      expect(new_name.full_name).to eq('America Calleros')
+      name = Person.new(first_name: 'Annabeth',last_name: 'Chase')
+      expect(name.full_name).to eq('Annabeth Chase')
     end
   end
 end
